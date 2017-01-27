a85cd1be7be369ff0ea44b160dbc97412412b8ff
Weerasak Chongnguluam <singpor@gmail.com>
2017-01-27 22:03:18 +0700

    Learn Rust TCP

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{self, Write, BufRead, BufReader};
+
+fn handle_client(stream: TcpStream) -> io::Result<()> {
+    write!(&stream, "Enter your name: ")?;
+    let mut f = BufReader::new(&stream);
+    let mut buffer = String::new();
+    f.read_line(&mut buffer)?;
+    writeln!(&stream, "Hello, {}!", buffer.trim())?;
+
+    Ok(())
+}
+
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream).unwrap();
+            }
+            Err(_) => {
+            }
+        }
+    }
+}
